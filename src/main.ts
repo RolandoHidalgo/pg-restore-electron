12cfe251394309704772a678a19b12f45b5553eb
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2022-09-10 00:52:43 -0400

    all done until vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
+import './core/bootstrapping/roboto-font-face.css'
+import {vuetify} from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
