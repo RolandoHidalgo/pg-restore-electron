660ebc5a1a64f88a55f7bcdfbf862eecb37b70c2
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2025-04-05 15:03:35 -0400

    Tailwind 4 migration (#8)
    * migration casi completa ya es seguro mezclar las ramas

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//import '@fontsource/geist-sans/100.css';
-//import '@fontsource/geist-sans/400.css';
-import '././assets/global.css'
 import '././assets/index.css'
 import { createPinia } from 'pinia'
 import {electronPluggin} from './plugins/electronPluggin'
