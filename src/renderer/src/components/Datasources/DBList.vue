566527a8f86055c69ca63715e844038f4819f5da
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2025-04-18 00:51:17 -0400

    -adicionando skeleton a los loadind de db y schemas -arrgelar el buscar db que tenia "startWith" y se cambio a "includes"

--- src/renderer/src/components/Datasources/DBList.vue
+++ src/renderer/src/components/Datasources/DBList.vue
@@ -29,14 +29,15 @@ import {
   DropdownMenuTrigger
 } from '@renderer/components/ui/dropdown-menu'
 import {computed,  watchEffect} from 'vue'
+import { Skeleton } from '@renderer/components/ui/skeleton'
 
 const props = defineProps<{ dsName: string,search:string }>()
-const { dbs } = useDbs(() => props.dsName)
+const { dbs,loadingDbs } = useDbs(() => props.dsName)
 const {openBackup,openRestore,openClone}=useApp()
 
 const currentDbs = computed(()=>{
   if(props.search!==''){
-    return dbs.value.filter(db=>db.startsWith(props.search))
+    return dbs.value.filter(db=>db.includes(props.search))
   }
   return dbs.value
 })
@@ -44,48 +45,74 @@ const currentDbs = computed(()=>{
 
 <template>
 
-  <SidebarMenuItem v-for="db in currentDbs" :key="db">
-    <Collapsible class="group/collapsible">
-      <CollapsibleTrigger as-child>
-        <SidebarMenuButton>
-          <Database class="text-blue-600"/>
-          {{ db }}
-          <Plus class="ml-auto group-data-[state=open]/collapsible:hidden" />
-          <Minus class="ml-auto group-data-[state=closed]/collapsible:hidden" />
-        </SidebarMenuButton>
+  <template v-if="!loadingDbs">
+    <SidebarMenuItem v-for="db in currentDbs" :key="db">
+      <Collapsible class="group/collapsible">
+        <CollapsibleTrigger as-child>
+          <SidebarMenuButton>
+            <Database class="text-blue-600"/>
+<!--           <span class="font-semibold"> {{ db }}</span>-->
+            {{ db }}
+            <Plus class="ml-auto group-data-[state=open]/collapsible:hidden " />
+            <Minus class="ml-auto group-data-[state=closed]/collapsible:hidden  " />
+          </SidebarMenuButton>
 
-      </CollapsibleTrigger>
-      <CollapsibleContent>
-        <SidebarMenuSub>
-          <SchemaList :ds-name="props.dsName" :db-name="db" />
-        </SidebarMenuSub>
-      </CollapsibleContent>
-    </Collapsible>
-    <DropdownMenu>
-      <DropdownMenuTrigger as-child>
-        <SidebarMenuAction show-on-hover>
-          <MoreHorizontal />
-          <span class="sr-only">More</span>
-        </SidebarMenuAction>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent class="w-26 rounded-lg" side="bottom" align="end">
-        <DropdownMenuItem @click="openBackup(props.dsName,db)">
-          <DatabaseBackup class="text-muted-foreground" />
-          <span >Backup</span>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem @click="openRestore(props.dsName,db)">
-          <DatabaseZap class="text-muted-foreground" />
-          <span>Restore</span>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem @click="openClone(props.dsName,db)">
-          <Copy class="text-muted-foreground" />
-          <span>Clone to</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </SidebarMenuItem>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            <SchemaList :ds-name="props.dsName" :db-name="db" />
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <SidebarMenuAction show-on-hover>
+            <MoreHorizontal />
+            <span class="sr-only">More</span>
+          </SidebarMenuAction>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-26 rounded-lg" side="bottom" align="end">
+          <DropdownMenuItem @click="openBackup(props.dsName,db)">
+            <DatabaseBackup class="text-muted-foreground" />
+            <span >Backup</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem @click="openRestore(props.dsName,db)">
+            <DatabaseZap class="text-muted-foreground" />
+            <span>Restore</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem @click="openClone(props.dsName,db)">
+            <Copy class="text-muted-foreground" />
+            <span>Clone to</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </SidebarMenuItem>
+  </template>
+  <template v-else>
+    <div class="flex items-center space-x-4 mb-3">
+      <Skeleton class="h-6 w-6 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+      <Skeleton class="h-3 w-3 rounded-full" />
+    </div>
+    <div class="flex items-center space-x-4 mb-3">
+      <Skeleton class="h-6 w-6 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+      <Skeleton class="h-3 w-3 rounded-full" />
+    </div>
+    <div class="flex items-center space-x-4 mb-3">
+      <Skeleton class="h-6 w-6 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+      <Skeleton class="h-3 w-3 rounded-full" />
+    </div>
+  </template>
 </template>
 
 <style scoped></style>
--- src/renderer/src/components/Datasources/SchemaList.vue
+++ src/renderer/src/components/Datasources/SchemaList.vue
@@ -8,9 +8,10 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@renderer/components/ui/dropdown-menu'
+import { Skeleton } from '@renderer/components/ui/skeleton'
 
 const props = defineProps<{ dsName: string; dbName: string }>()
-const { schemas } = useSchemas(
+const { schemas,loadingSchemas } = useSchemas(
   () => props.dsName,
   () => props.dbName
 )
@@ -18,26 +19,44 @@ const {openBackup}=useApp()
 </script>
 
 <template>
-  <SidebarMenuItem v-for="schema in schemas" :key="schema">
-    <SidebarMenuButton>
-      <Network class="text-blue-500"/>
-      {{ schema }}
-    </SidebarMenuButton>
-    <DropdownMenu>
-      <DropdownMenuTrigger as-child>
-        <SidebarMenuAction show-on-hover>
-          <MoreHorizontal />
-          <span class="sr-only">More</span>
-        </SidebarMenuAction>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent class="w-26 rounded-lg" side="bottom" align="end">
-        <DropdownMenuItem @click="openBackup(props.dsName,props.dbName,schema)">
-          <DatabaseBackup class="text-muted-foreground" />
-          <span>Backup</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </SidebarMenuItem>
+  <template v-if="!loadingSchemas">
+    <SidebarMenuItem v-for="schema in schemas" :key="schema">
+      <SidebarMenuButton>
+        <Network class="text-blue-500"/>
+        {{ schema }}
+      </SidebarMenuButton>
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <SidebarMenuAction show-on-hover>
+            <MoreHorizontal />
+            <span class="sr-only">More</span>
+          </SidebarMenuAction>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-26 rounded-lg" side="bottom" align="end">
+          <DropdownMenuItem @click="openBackup(props.dsName,props.dbName,schema)">
+            <DatabaseBackup class="text-muted-foreground" />
+            <span>Backup</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </SidebarMenuItem>
+  </template>
+  <template v-else>
+    <div class="flex items-center space-x-4 mb-3">
+      <Skeleton class="h-4 w-4 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-3 w-[150px]" />
+      </div>
+      <Skeleton class="h-3 w-3 rounded-full ml-auto" />
+    </div>
+    <div class="flex items-center space-x-4 mb-3">
+      <Skeleton class="h-4 w-4 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-3 w-[150px]" />
+      </div>
+      <Skeleton class="h-3 w-3 rounded-full ml-auto" />
+    </div>
+  </template>
 </template>
 
 <style scoped></style>
